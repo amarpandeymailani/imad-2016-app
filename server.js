95dbae6e1ed49ba596bbc1aae95abdca47208261
amarpandeymailani <amarpandey.mailani@gmail.com>
2016-09-27 20:09:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,8 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article1', function (req, res) {
-  res.send('this is what i want1');
+  res.sendFile(path.join(__dirname, 'ui', 'article1.html'));
+
 }); 
 app.get('/article2', function (req, res) {
   res.send('this is what i want2');
