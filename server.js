1199c2e4c9e07833ef073f0bbf55bd62be0bc37a
amarpandeymailani <amarpandey.mailani@gmail.com>
2016-09-28 17:58:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,11 +12,11 @@ app.get('/article-one', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 
 }); 
-app.get('/article2', function (req, res) {
-  res.send(path.join(__dirname, 'ui', 'article2.html'));
+app.get('/article-two', function (req, res) {
+  res.send(path.join(__dirname, 'ui', 'article-two.html'));
 }); 
-app.get('/article3', function (req, res) {
-  res.send(path.join(__dirname, 'ui', 'article3.html'));
+app.get('/article-three', function (req, res) {
+  res.send(path.join(__dirname, 'ui', 'article-three.html'));
 }); 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
