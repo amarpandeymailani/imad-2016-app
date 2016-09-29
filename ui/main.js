43baf6b2cf61a0e29e5984a20d07b487a7814465
amarpandeymailani <amarpandey.mailani@gmail.com>
2016-09-29 18:05:46 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,7 +3,7 @@ console.log('Loaded!');
 var element=document.getElementById('main-text');
 element.innerHTML= 'NEW VALUE';
 //move the image
-var element=document.getElementById('img');
+var element=document.getElementById('madi');
 img.onclick = function(){
     img.style.marginLeft='100px';
 };
