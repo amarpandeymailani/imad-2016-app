13cb91a4255e9a636892be857450c73f35fb112d
amarpandeymailani <amarpandey.mailani@gmail.com>
2016-10-02 22:47:10 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,14 +2,27 @@
 var button = document.getElementById('counter');
 var counter = 0;
 button.onclick = function () {
-    //make request to a counter end point
+    //craete a request object
+    var request = newXMLHttpRequest();
+    
     
     //capture the response and store it to in a variable
+    request.onreadystatechange = function() {
+      if(request.readystate===XMLHttpRequest.done)  {
+          //take some action 
+          if (request.status===200){
+            var span = document.getElementById('count');
+            span.innerHTML = counter.toString();
+              
+          }
+          
+      }
+      //not done yet
+    };
     
-    //render the variable in the correct span
-     counter = counter + 1;
-     var span = document.getElementById('count');
-     span.innerHTML = counter.toString();
+   //make the request
+   request.open('GET',"http://amarpandeymailani.imad.hasura-app.io/counter",true);
+   request.send(null);
      
 };
      
