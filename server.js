aabc87a7baf4ced16e306f213c3f7426c032047f
amarpandeymailani <amarpandey.mailani@gmail.com>
2016-10-11 00:45:01 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -106,9 +106,9 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 var names = [];
-app.get('/submit-name/:name', function (req, res) { //url submit-name?name= xxxx
+app.get('/submit-name', function (req, res) { //url submit-name?name= xxxx
     //get the name from the req.
-    var name = req.params.name;
+    var name = req.query.name;
     names.push(JSON.stringify(name));
     //json javascript object notation   
   
