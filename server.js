7e69a6701435e1e967f922b424ae4906803522c8
amarpandeymailani <amarpandey.mailani@gmail.com>
2016-11-06 19:11:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -118,11 +118,28 @@ app.get('/submit-name', function (req, res) { //url submit-name?name= xxxx
 });
 
 
-app.get('/:articleName', function (req, res) {
+app.get('/articles/:articleName', function (req, res) {
     //articleName=article-one
     //articles[articleName]=content object for articleone
-    var articleName=req.params.articleName;
-  res.send(createTemplate(articles[articleName]));
+    
+    
+  pool.query("SELECT * FROM article WHERE title = " + req.params.articleName ,function(err,result){
+if(err){
+    res.status(500).send(err.toString());
+    
+} else {
+      if(result.rows.length === 0){
+          res.status(404).send('article not found');
+          
+      }
+      else{
+          var articleData = result.rows[0];
+          res.send(createTemplate(articleData));
+      }
+    
+}
+  });
+  res.send(createTemplate(articleData));
 });
 
 
