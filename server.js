8859311062b3c76339b4583871729a66bc499a1c
amarpandeymailani <amarpandey.mailani@gmail.com>
2016-11-06 19:17:14 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -122,8 +122,8 @@ app.get('/articles/:articleName', function (req, res) {
     //articleName=article-one
     //articles[articleName]=content object for articleone
     
-    
-  pool.query("SELECT * FROM article WHERE title = " + req.params.articleName ,function(err,result){
+  //select * from   article where title = 'article-one'
+  pool.query("SELECT * FROM article WHERE title = '" + req.params.articleName + "'" ,function(err,result){
 if(err){
     res.status(500).send(err.toString());
     
